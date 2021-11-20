5bad463f92a400252a31f7df4a8dd97c5358ec45
Oksana <briliant621621@gmail.com>
2021-11-20 18:55:04 +0200

    Adeed hello java class

---
+++ src/main/java/HelloJava.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class HelloJava {
+    public static void main (String [] args) {
+        System.out.println ("HelloJava");
+        System.out.println("This");
+    }
+}
