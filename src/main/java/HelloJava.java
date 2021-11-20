fb24900b55f773b2480c3a087481899e99f6a0b4
Oksana <briliant621621@gmail.com>
2021-11-20 19:02:10 +0200

    Changed HelloJava.java

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -3,6 +3,6 @@ package main.java;
 public class HelloJava {
     public static void main (String [] args) {
         System.out.println ("HelloJava");
-        System.out.println("This");
+        System.out.println("This is our second commit");
     }
 }
